caf0e3d42106cc68426fffec5737ca7cd7d47fee
takanashi jugemu <jugemu@exam.co.jp>
2015-12-14 05:02:26 +0000

    seed変更～

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to @product, notice: '項目は正常に作成されたバイ' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to @product, notice: '項目は正常に更新されたバイ' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,50 +13,103 @@ Product.delete_all
 Product.create([
   {
     id: 1,
-    title: 'Programing Ruby 1.9',
+    title: '蔵で臼',
     description: %{<p>
-      Ruby is the fastest growing 
-      and most exciting dynamic language out there.
+      懐かしの横スクロールシューティングゲーム。</p>
+      <p>蔵の中で次々と襲ってくる古物を撃ち落とせ！
       </p>},
-    image_url: 'ruby.jpg',
-    price: 49.95
+    image_url: '130723Gradius-thumb-640x393-74491.jpg',
+    price: 5600
   },
   {
     id: 2,
-    title: 'CoffeeScript',
+    title: 'FirstFantasy',
     description: %{<p>
-      CoffeeScript is JavaScript done right.
+      世界を動かしている4つの水晶が砕かれようとしている。</p>
+      <p>世界の平和を守るため、水晶を守る冒険が今始まる・・・
       </p>},
-    image_url: 'cs.jpg',
-    price: 36.00
+    image_url: 'FF.png',
+    price: 6400
   },
   {
     id: 3,
-    title: 'Rails Test Prescriptions',
+    title: '竜探索',
     description: %{<p>
-      Rails Test Prescriptions is a comprehensive guide to testing.
+      突如として竜達が暴れまわり、街を破壊し始めた！</p>
+      <p>暴れる竜を打ち倒し、世界の平和を取り戻すため、竜王を倒せ！
       </p>},
-    image_url: 'rtp.jpg',
-    price: 45.00
+    image_url: 'dq.jpg',
+    price: 6100
   },
   {
     id: 4,
-    title: 'ハートでもできる北斗神拳',
+    title: 'どきどきメモリアル',
     description: %{<p>
-      奥義も含めて北斗神拳の使い方を掲載！
-      </p>
-      <P>これであなたも北斗神拳伝承者に</p>},
-    image_url: 'hyakuretuken.jpg',
-    price: 100.015
+      楽しい高校生活の始まり・・・</p>
+      <p>隣に住む幼馴染や同級生・部活の先輩など魅力的な女の子が一杯！</p>
+      <p>好みの女の子と仲良くなれるかどうかは貴方の行動次第
+      </p>},
+    image_url: 'smile.jpg',
+    price: 5800
   },
   {
     id: 5,
-    title: 'チーズカツカレーの極意',
+    title: '道三の野望',
     description: %{<p>
-      海原雄山・山岡士郎・荒岩一味など著名人のチーズカツカレーのレシピを公開！</p>
-      <p>貴方も最高のチーズカツカレーを食べてみよう♪
+      油売りから一国一城の主となった斉藤道三</p>
+      <p>道半ばにして敗れた道三の野望を叶えるため全国統一を果たそう！
       </p>},
-    image_url: 'curry.jpg',
-    price: 65.05
+    image_url: 'pc_koe_000833_01.jpg',
+    price: 9800
+  },
+  {
+    id: 6,
+    title: 'ぶよぶよ',
+    description: %{<p>
+      あのゲームの人気キャラクターがパズルゲームに登場！</p>
+      <p>お餅のような【ぶよぶよ】を縦・横に並べて消そう♪
+      </p>},
+    image_url: '38730.jpg',
+    price: 5200
+  },
+  {
+    id: 7,
+    title: '熱狂パワフルプロ野球',
+    description: %{<p>
+      自分のプロ野球チームを編成して日本一を目指せ！</p>
+      <p>自分がプロ野球選手になって人生を過ごす【マイ人生】モードも搭載♪
+      </p>},
+    image_url: 'img_pawapuro.png',
+    price: 7800
+  },
+  {
+    id: 8,
+    title: 'Wining11人',
+    description: %{<p>
+      自分の好きなチームでJリーグ優勝、そしてCWC優勝を勝ち取ろう！</p>
+      <p>オリジナルプレイヤーを作成して、育てることもできるぞ♪
+      </p>},
+    image_url: 'PES2011_kickoff_bmp_jpgcopy.jpg',
+    price: 7500
+  },
+  {
+    id: 9,
+    title: 'アブラゼミのなく頃に',
+    description: %{<p>
+      アブラゼミが鳴く季節に起こる惨劇・・・</p>
+      <p>貴方はこの謎を解き明かし、惨劇を回避できるか？
+      </p>},
+    image_url: '8447857_624.jpg',
+    price: 6400
+  },
+  {
+    id: 10,
+    title: 'ファンタシームーン',
+    description: %{<p>
+      平和な毎日を過ごしていた主人公</p>
+      <p>しかし、突如として平和な日々が乱されることとなる・・・
+      </p>},
+    image_url: '8tnu0100004i1y44.jpg',
+    price: 6900
   }
   ])
